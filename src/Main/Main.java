c805d5c07c4278d747f0a9acced5251ab9f2920e
Andres Tambek <Tambekandres@gmail.com>
2016-10-06 20:21:35 +0300

    suva commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Silla ületamine...");
         System.out.println("Autor: Andres Tambek");
-	System.out.println("Esimesed katsetused Javas");
-	System.out.println("Aasta: 2016");
+	    System.out.println("Esimesed katsetused Javas");
+	    System.out.println("Aasta: 2016");
     }
 }
