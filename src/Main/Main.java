e4dbaa17bd1985afa0243540a34a8d778844a27d
Andres Tambek <Tambekandres@gmail.com>
2016-10-06 18:59:26 +0300

    logi katsetamine

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Silla ületamine...");
+        System.out.println("Silla Ã¼letamine...");
         System.out.println("Autor: Andres Tambek");
+		System.out.println("Esimesed katsetused Javas");
     }
 }
